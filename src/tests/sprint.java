3a6b2afacda514e9a1138e259395af3c4627ea71
“enesberber” <enes-berber3321@hotmail.com>
2020-05-06 18:16:19 +0200

    update sprint.java

--- src/tests/sprint.java
+++ src/tests/sprint.java
@@ -4,5 +4,6 @@ public class sprint {
     public static void main(String[] args) {
         System.out.println("Helloooo");
         System.out.println("Whatuppp");
-    }
+        System.out.println("Fine and you");
+}
 }
