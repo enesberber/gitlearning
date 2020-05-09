2a0a6717433f3c46e59ae0096a53772f753f0a37
“enesberber” <enes-berber3321@hotmail.com>
2020-05-09 22:27:04 +0200

    add batuhan.java

--- src/tests/batuhan.java
+++ src/tests/batuhan.java
@@ -5,6 +5,9 @@ public class batuhan {
     public static void main(String[] args) {
         System.out.println("batuhan neden daha onceden yataga yatmiyorsun");
         System.out.println("Cabuk yataga");
+        System.out.println("deneme");
+
+
     }
 
 }
