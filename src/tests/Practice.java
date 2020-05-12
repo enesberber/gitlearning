8b341c022e585d5af195574f0b89e8fdad6a9d9e
enesberber <62036447+enesberber@users.noreply.github.com>
2020-05-12 17:08:36 +0200

    Update Practice.java

--- src/tests/Practice.java
+++ src/tests/Practice.java
@@ -4,5 +4,6 @@ public class Practice {
     public static void main(String[] args) {
         System.out.println("jsdjsadjsdajsadj");
         System.out.println("sadsda");
+        System.out.println("This line written from github");
     }
 }
