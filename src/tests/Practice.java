7cf64bc789970810248c73ca59604feb7b2d85a8
enesberber <62036447+enesberber@users.noreply.github.com>
2020-05-12 17:07:38 +0200

    Update Practice.java

--- src/tests/Practice.java
+++ src/tests/Practice.java
@@ -3,6 +3,6 @@ package tests;
 public class Practice {
     public static void main(String[] args) {
         System.out.println("jsdjsadjsdajsadj");
-
+        System.out.println("sadsda");
     }
 }
